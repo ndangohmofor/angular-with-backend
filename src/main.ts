5270a6088157014f3ffc0969d35c6d257d3fd5bc
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-04-01 08:48:09 -0400

    implement and http interceptor and add headers to outgoing requests

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
+function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const request = req.clone({
+    headers: req.headers.set('X-DEBUG', 'TESTING'),
+  });
+  console.log('[Outgoing Request: ]', req);
+  return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch((err) => console.error(err));
