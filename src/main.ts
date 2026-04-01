8dd5449c6fab83e86f26af5d015eb0268ab299f7
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-04-01 08:56:42 -0400

    add the response interceptors and log the incoming response status and body

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,30 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
-  const request = req.clone({
-    headers: req.headers.set('X-DEBUG', 'TESTING'),
-  });
+  //   const request = req.clone({
+  //     headers: req.headers.set('X-DEBUG', 'TESTING'),
+  //   });
   console.log('[Outgoing Request: ]', req);
-  return next(request);
+  return next(req).pipe(
+    tap({
+      next: (event) => {
+        if (event?.type === HttpEventType.Response) {
+          console.log('[Incoming Response: ]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    }),
+  );
 }
 
 bootstrapApplication(AppComponent, {
